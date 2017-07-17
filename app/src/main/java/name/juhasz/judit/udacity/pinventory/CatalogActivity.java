420b359225f8ce9410f615bb540280a9b4b1809e
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-07-17 11:03:31 +0200

    Use an empty Android Studio project as base

---
+++ app/src/main/java/name/juhasz/judit/udacity/pinventory/CatalogActivity.java
@@ -0,0 +1,13 @@
+package name.juhasz.judit.udacity.pinventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+    }
+}
