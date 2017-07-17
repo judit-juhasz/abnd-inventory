9d25417ee51a5d2a401469b367fad977ee14b60d
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-07-17 12:16:18 +0200

    Remove test data filler functionality

--- app/src/main/java/name/juhasz/judit/udacity/pinventory/CatalogActivity.java
+++ app/src/main/java/name/juhasz/judit/udacity/pinventory/CatalogActivity.java
@@ -1,23 +1,15 @@
 package name.juhasz.judit.udacity.pinventory;
 
 import android.app.LoaderManager;
-import android.content.ContentValues;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-
-import java.io.ByteArrayOutputStream;
 
 import name.juhasz.judit.udacity.pinventory.data.PinventoryContract.PinventoryEntry;
 
@@ -53,25 +45,6 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_catalog, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_insert_dummy_data:
-                insertProduct();
-                return true;
-            case R.id.action_delete_all_entries:
-                deleteAllProduct();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         String[] projection = {
                 PinventoryEntry._ID,
@@ -95,37 +68,5 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         mCursorAdapter.swapCursor(null);
-    }
-
-    private void insertProduct() {
-        ContentValues values = new ContentValues();
-
-        values.put(PinventoryEntry.COLUMN_PRODUCT_NAME, "Phone");
-        values.put(PinventoryEntry.COLUMN_PRODUCT_PRICE, 120);
-        values.put(PinventoryEntry.COLUMN_PRODUCT_QUANTITY, 5);
-        values.put(PinventoryEntry.COLUMN_PRODUCT_SUPPLIER_EMAIL, "judit@juhasz.name");
-        BitmapDrawable drawable = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            drawable = (BitmapDrawable) getDrawable(R.drawable.image_empty_shelter);
-        }
-        byte[] imageBytes = getBytes(drawable.getBitmap());
-        values.put(PinventoryEntry.COLUMN_PRODUCT_IMAGE, imageBytes);
-
-        getContentResolver().insert(PinventoryEntry.CONTENT_URI, values);
-    }
-
-    private static byte[] getBytes(Bitmap bitmap) {
-        if (bitmap != null) {
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
-            return stream.toByteArray();
-        } else {
-            return null;
-        }
-    }
-
-    private void deleteAllProduct() {
-        int rowsDeleted = getContentResolver().delete(PinventoryEntry.CONTENT_URI, null, null);
-        Log.v("CatalogActivity", rowsDeleted + " rows deleted from pet database");
     }
 }
